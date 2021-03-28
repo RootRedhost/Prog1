7228f8072d96e6841419324fe07709e82c53c0e2
RootRedhost <36854541+RootRedhost@users.noreply.github.com>
2021-03-28 13:20:10 +0200

    Add files via upload

--- Drill2/main.cpp
+++ Drill2/main.cpp
@@ -1,71 +1,59 @@
-#include "std_lib_facilities.h"
-
-class B1 {
-public:
-  virtual void vf() { cout << "B1::vf()\n";}
-  void f(){ cout << "B1::f()\n";}
-  virtual void pvf() = 0;
-};
-
-class D1 : public B1 {
-public:
-  void vf() { cout << "D1::vf()\n";}
-  void f() { cout << "D1::f()\n";}
-};
-
-class D2 : public D1 {
-public:
-  void pvf() { cout << "D2::pvf()\n";}
-};
-
-class B2 {
-public:
-  virtual void pvf() = 0;
-};
-
-class D21 : public B2 {
-public:
-  void pvf() { cout << s << endl;}
-  string s;
-};
-
-class D22 : public B2 {
-public:
-  void pvf() { cout << i << endl;}
-  int i;
-};
-
-void f(B2& b2) {
-  b2.pvf();
-};
+/*
+    g++ main.cpp Graph.cpp Window.cpp GUI.cpp Simple_window.cpp -o main `fltk-config --ldflags --use-images`
+*/
+#include "Simple_window.h"
+#include "Graph.h"
 
 int main(){
-  /*
-  B1 b1;
-  b1.vf();
-  b1.f();
+  Simple_window win {Point{100,100},1000,800,"Window"}; //ablak 800x1000
 
-  D1 d1;
-  d1.vf();
-  d1.f();
+  int x_size = win.x_max();
+  int y_size = win.y_max();
+  int x_grid = 100;
+  int y_grid = 100;
+  Lines grid;
 
-  B1& bref = d;
-  bref.vf();
-  bref.f();
-  */
-  D2 d2;
-  d2.f();
-  d2.vf();
-  d2.pvf();
+  for (int x = x_grid; x < x_size-100; x += x_grid)
+    grid.add(Point{x,0},Point{x,y_size});
 
-  D21 d21;
-  d21.s = "d21.s";
+  for (int y = y_grid; y < y_size; y += y_grid)
+    grid.add(Point{0,y},Point{x_size-200,y});
 
-  D22 d22;
-  d22.i = 22;
+  Vector_ref<Rectangle> re;
+  for (int i = 0;i < 8; ++i)
+  {
+      re.push_back(new Rectangle{Point{i*100,i*100},100,100});
+      re[re.size()-1].set_fill_color(Color::red);
+      win.attach(re[re.size()-1]);
+    }
 
-  f(d21);
-  f(d22);
+  Image twitch0 {Point{0,600},"twitch200.jpeg"};
+  Image twitch1 {Point{300,0},"twitch200.jpeg"};
+  Image twitch2 {Point{500,200},"twitch200.jpeg"};
 
-  keep_window_open();
+  Image twitch100 {Point{0,0},"twitch100.jpg"};
+
+
+  win.attach(twitch0);
+  win.attach(twitch1);
+  win.attach(twitch2);
+  win.attach(grid);
+  win.attach(twitch100);
+  win.wait_for_button();
+
+  int x = 0;
+  int y = 0;
+  int dx = 0;
+  int dy = 0;
+  while (true) {
+    x = randint(7);
+    y = randint(7);
+    dx = 100*x - twitch100.point(0).x;
+    dy = 100*y - twitch100.point(0).y;
+    twitch100.move(dx,dy);
+
+    win.wait_for_button();
+  }
+
+  return 0;
 }
